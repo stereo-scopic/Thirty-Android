555631358d341e1ba6c19032275a019328bd4725
SongHaeun <she000516@ai-car.co.kr>
2022-12-15 11:01:34 +0900

    [FEAT] 갤러리 접근 및 사진 첨부

--- app/src/main/java/com/example/thrity/ChallengePost.kt
+++ app/src/main/java/com/example/thrity/ChallengePost.kt
@@ -2,6 +2,9 @@ package com.example.thrity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import com.example.thrity.databinding.ActivityChallengePostBinding
 
 class ChallengePost : AppCompatActivity() {
@@ -12,8 +15,30 @@ class ChallengePost : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
 
+        checkPermission.launch(permissionList)
+
+        viewBinding.btnCamera.setOnClickListener {
+            readImage.launch("image/*")
+        }
+
         viewBinding.btnBack.setOnClickListener {
             finish()
         }
+    }
+
+    private val permissionList = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    private val checkPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+        result.forEach {
+            if(!it.value) {
+                Toast.makeText(applicationContext, "권한 동의 필요", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private val readImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        Glide.with(this)
+            .load(uri)
+            .into(viewBinding.image)
     }
 }
