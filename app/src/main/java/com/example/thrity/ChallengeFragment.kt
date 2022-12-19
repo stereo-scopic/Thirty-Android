9e490bb7d1d96e60d297020d52e5b959bc17d507
SongHaeun <she000516@ai-car.co.kr>
2022-12-19 12:27:38 +0900

    [FEAT] Challenge 화면 Grid 생성

--- app/src/main/java/com/example/thrity/ChallengeFragment.kt
+++ app/src/main/java/com/example/thrity/ChallengeFragment.kt
@@ -6,9 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewbinding.ViewBinding
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.thrity.databinding.FragmentChallengeBinding
-import com.example.thrity.databinding.FragmentExploreBinding
 
 class ChallengeFragment: Fragment() {
     private lateinit var viewBinding: FragmentChallengeBinding
@@ -23,6 +22,21 @@ class ChallengeFragment: Fragment() {
             val intent = Intent(context, ChallengePost::class.java)
             startActivity(intent)
         }
+
+        //grid 번갈아 그리기
+        val list: ArrayList<ChallengeGridData> = arrayListOf()
+        list.apply{
+            for(i: Int in 1..30){
+                add(ChallengeGridData("$i"))
+            }
+        }
+
+        val listManager = GridLayoutManager(context, 6)
+        val listAdapter = ListChallengeGridAdaptor(list)
+        viewBinding.rvChallenge.adapter = listAdapter
+        viewBinding.rvChallenge.layoutManager = listManager
+        viewBinding.rvChallenge.addItemDecoration(ItemDecorator(0))
+
         return viewBinding.root
     }
 }
--- app/src/main/java/com/example/thrity/ListChallengeGridAdaptor.kt
+++ app/src/main/java/com/example/thrity/ListChallengeGridAdaptor.kt
@@ -1,7 +1,11 @@
 package com.example.thrity
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.thrity.databinding.ChallengeGridItemBinding
 
@@ -10,15 +14,29 @@ class ListChallengeGridAdaptor (var challengeGridList: ArrayList<ChallengeGridDa
     inner class ChallengeGridDataViewHolder(private val viewBinding: ChallengeGridItemBinding) : RecyclerView.ViewHolder(viewBinding.root) {
         fun bind(data: ChallengeGridData) = with(viewBinding){
             tvGridNum.text = data.num
+            if(tvGridNum.text.toString().toInt() % 6 == 0){
+                if (tvGridNum.text.toString().toInt() / 6 != 2 && tvGridNum.text.toString().toInt() / 6 != 4){
+                    grid.setBackgroundColor(Color.BLACK)
+                    tvGridNum.setTextColor(Color.WHITE)
+                }
+            }else{
+                if (tvGridNum.text.toString().toInt() / 6 == 1 || tvGridNum.text.toString().toInt() / 6 == 3){
+                    if (tvGridNum.text.toString().toInt() % 2 != 0){
+                        grid.setBackgroundColor(Color.BLACK)
+                        tvGridNum.setTextColor(Color.WHITE)
+                    }
+                }else{
+                    if (tvGridNum.text.toString().toInt() % 2 == 0){
+                        grid.setBackgroundColor(Color.BLACK)
+                        tvGridNum.setTextColor(Color.WHITE)
+                    }
+                }
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChallengeGridDataViewHolder {
         val layout = ChallengeGridItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-//        val grid = ()
-//        if ((layout.tvGridNum)%2 == 0){
-//            layout.grid.SetBackgroundColor
-//        }
         return  ChallengeGridDataViewHolder(layout)
     }
 
@@ -26,5 +44,7 @@ class ListChallengeGridAdaptor (var challengeGridList: ArrayList<ChallengeGridDa
         holder.bind(challengeGridList[position])
     }
 
-    override fun getItemCount(): Int = challengeGridList.size
+    override fun getItemCount(): Int {
+        return challengeGridList.size
+    }
 }
