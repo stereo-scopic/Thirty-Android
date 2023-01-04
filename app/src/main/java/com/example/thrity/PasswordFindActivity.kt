66e649c19b8a1d8f7091e98fa47d7c0706f8d36e
SongKong00 <hesong1@naver.com>
2023-01-04 11:09:39 +0900

    [FEAT[ 비밀번호 재설정 완료 화면 생성

---
+++ app/src/main/java/com/example/thrity/PasswordFindActivity.kt
@@ -0,0 +1,11 @@
+package com.example.thrity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PasswordFindActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_find)
+    }
+}
